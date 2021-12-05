f80dc5d42f84baeea814a29dde115be608eb5102
Umang Srivastav <usrivastav10@gmail.com>
2021-12-04 19:21:05 -0800

    updated website to include merchandise and trial run of routing

--- index.php
+++ index.php
@@ -1,1 +1,19 @@
-<?php include_once("home.html"); ?>
+<?php
+
+$request = $_SERVER['REQUEST_URI'];
+
+switch ($request) {
+    case '/' :
+        include_once("home.html");
+        break;
+    case '' :
+        include_once("home.html");
+        break;
+    case '/about' :
+        include_once("about.html");
+        break;
+    default:
+        http_response_code(404);
+        include_once("home.html");
+        break;
+} ?>
